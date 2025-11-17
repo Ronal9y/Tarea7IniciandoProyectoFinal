5e4a73f79b39591d56524e9b1bd46c6afb84f806
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-11-16 20:10:30 -0400

    agregando temas de material3

--- app/src/main/java/edu/ucne/tarea7iniciandoproyectofinal/ui/theme/Theme.kt
+++ app/src/main/java/edu/ucne/tarea7iniciandoproyectofinal/ui/theme/Theme.kt
@@ -9,18 +9,65 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFFA5C8FF),
+    onPrimary = Color(0xFF003063),
+    primaryContainer = Color(0xFF00478B),
+    onPrimaryContainer = Color(0xFFD5E3FF),
+    inversePrimary = Color(0xFF005DB4),
+    secondary = Color(0xFFBEC6DC),
+    onSecondary = Color(0xFF283041),
+    secondaryContainer = Color(0xFF3E4759),
+    onSecondaryContainer = Color(0xFFDAE2F9),
+    tertiary = Color(0xFFDEBCDF),
+    onTertiary = Color(0xFF402743),
+    tertiaryContainer = Color(0xFF583D5B),
+    onTertiaryContainer = Color(0xFFFBD7FC),
+    background = Color(0xFF1B1B1D),
+    onBackground = Color(0xFFE3E2E6),
+    surface = Color(0xFF1B1B1D),
+    onSurface = Color(0xFFE3E2E6),
+    surfaceVariant = Color(0xFF43474F),
+    onSurfaceVariant = Color(0xFFC3C6CF),
+    inverseSurface = Color(0xFFE3E2E6),
+    inverseOnSurface = Color(0xFF2F3033),
+    outline = Color(0xFF8D9199),
+    error = Color(0xFFFFB4AB),
+    onError = Color(0xFF690005),
+    errorContainer = Color(0xFF93000A),
+    onErrorContainer = Color(0xFFFFDAD6)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF005DB4),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFFD5E3FF),
+    onPrimaryContainer = Color(0xFF001B3D),
+    inversePrimary = Color(0xFFA5C8FF),
+    secondary = Color(0xFF565E71),
+    onSecondary = Color(0xFFFFFFFF),
+    secondaryContainer = Color(0xFFDAE2F9),
+    onSecondaryContainer = Color(0xFF131C2C),
+    tertiary = Color(0xFF705574),
+    onTertiary = Color(0xFFFFFFFF),
+    tertiaryContainer = Color(0xFFFBD7FC),
+    onTertiaryContainer = Color(0xFF29132E),
+    background = Color(0xFFFDFBFF),
+    onBackground = Color(0xFF1B1B1D),
+    surface = Color(0xFFFDFBFF),
+    onSurface = Color(0xFF1B1B1D),
+    surfaceVariant = Color(0xFFE0E2EC),
+    onSurfaceVariant = Color(0xFF43474F),
+    inverseSurface = Color(0xFF2F3033),
+    inverseOnSurface = Color(0xFFF2F0F4),
+    outline = Color(0xFF74777F),
+    error = Color(0xFFBA1A1A),
+    onError = Color(0xFFFFFFFF),
+    errorContainer = Color(0xFFFFDAD6),
+    onErrorContainer = Color(0xFF410002)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -32,6 +79,7 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Color(0xFF1C1B1F),
     */
 )
+
 
 @Composable
 fun Tarea7IniciandoProyectoFinalTheme(
